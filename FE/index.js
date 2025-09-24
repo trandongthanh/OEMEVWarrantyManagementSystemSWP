d5c610124fdb1bd44bbd40f9ebb36f713a0b40ac
danggiabao2005 <baongu2005@gmail.com>
2025-09-24 21:56:48 +0700

    update xoa index.js

--- FE/index.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello world");
-console.log("tuitenlagiahuydeptraiso1tg");
-console.log("tuitenlagiahuydeptraiso2tg");
-console.log("tuitenlagiahuydeptraiso3tg");
-console.log("tuitenlagiahuydeptraiso4tg");
-console.log("tuitenlagiahuydeptraiso5tg");
