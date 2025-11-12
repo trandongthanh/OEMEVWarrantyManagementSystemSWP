5d52a2bae3632e4175e619a63950f5aa5b8e352c
danggiabao2005 <baongu2005@gmail.com>
2025-11-12 23:27:34 +0700

    change api url

--- FE/src/components/PartsCompanyDashboard.tsx
+++ FE/src/components/PartsCompanyDashboard.tsx
@@ -22,7 +22,7 @@ import {
   CheckCircle
 } from "lucide-react";
 
-const API_BASE_URL = 'http://localhost:3000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 interface StockTransferRequest {
   id: string;
--- FE/src/components/PartsCoordinatorDashboard.tsx
+++ FE/src/components/PartsCoordinatorDashboard.tsx
@@ -34,7 +34,7 @@ import {
   Search
 } from "lucide-react";
 
-const API_BASE_URL = 'http://localhost:3000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 interface TypeComponent {
   typeComponentId: string;
